19479cecbaa0515710d6e9388a1e1711a054c215
Freezer05 <69045912+Freezer053053@users.noreply.github.com>
2025-01-21 14:29:18 +0100

    Update reto4.cpp

--- primerospasos/retos/reto4.cpp
+++ primerospasos/retos/reto4.cpp
@@ -2,29 +2,41 @@
 using namespace std;
 
 int main(){
+
     int numero;
-    
+
     cout<<"Introduce el numero de elementos: ";
 
     cin>>numero;
 
     int numeros[numero];
-    
-    cout<<"Introduce "<<numero<<" numeros:"<<endl;
 
-    for(int i=0; i<numero; i++){
+    cout<<"Introduce "<<numero<<" numeros:"<< endl;
+
+    for(int i=0; i<numero; i++) {
 
         cin>>numeros[i];
-    }
-
-    cout<<"Numeros en orden inverso:"<<endl;
-
-    for(int i=0; i<numero; i++){
-
-        cout<<numeros[numero-1-i]<<" ";
 
     }
 
-    cout<<endl;
 
+    for(int i=0; i<numero/2; i++) {
+
+        numeros[i]=numeros[i]+numeros[numero-1-i];
+
+        numeros[numero-1-i]=numeros[i]-numeros[numero-1-i];
+
+        numeros[i]=numeros[i]-numeros[numero-1-i];
+
+    }
+
+    cout << "Array invertido:" << endl;
+
+    for(int i = 0; i < numero; i++) {
+
+        cout << numeros[i] << " ";
+
+    }
+
+    cout << endl;
 }
